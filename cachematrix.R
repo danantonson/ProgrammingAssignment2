5bb454282634b097eeaf1d7312bc5263ac1360be
danantonson <danmantonson@gmail.com>
2015-04-26 16:13:52 -0500

    Commiting Final Code for Assign. Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the code for Coursera Assignment 2. 
+## The goal is to cache a matrix with a function, and 'solve' the matrix with another function
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the initial cache of the matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
 
 
-
-## Write a short comment describing this function
+## This the function that 'solves' makeCacheMatrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get ()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
